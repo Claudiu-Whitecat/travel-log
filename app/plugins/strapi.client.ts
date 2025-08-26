3f013aed59141e3dee7e617df8b8f31b38afefe2
claudiu.vimes <claudiu@whitecat.ro>
2025-08-26 13:59:14 +0300

    added stuff

--- app/plugins/strapi.client.ts
+++ app/plugins/strapi.client.ts
@@ -1,16 +1,16 @@
-import type { Strapi4Error } from '@nuxtjs/strapi/dist/runtime/types/v4'
-
-export default defineNuxtPlugin((nuxt) => {
-    const toast = useToast();
-
-    nuxt.hook('strapi:error' as any, (e: Strapi4Error) => {
-        const errors = {
-        };
-
-        const customError = errors?.[e.error.name]?.[e.error.message];
-        const title = customError?.title || e.error.name;
-        const message = customError?.message || e.error.message;
-
-        toast.add({ title: 'Ooops!', description: message });
-    });
-});
+// import type { Strapi4Error } from '@nuxtjs/strapi/dist/runtime/types/v4'
+//
+// export default defineNuxtPlugin((nuxt) => {
+//     const toast = useToast();
+//
+//     nuxt.hook('strapi:error' as any, (e: Strapi4Error) => {
+//         const errors = {
+//         };
+//
+//         const customError = errors?.[e.error.name]?.[e.error.message];
+//         const title = customError?.title || e.error.name;
+//         const message = customError?.message || e.error.message;
+//
+//         toast.add({ title: 'Ooops!', description: message });
+//     });
+// });
